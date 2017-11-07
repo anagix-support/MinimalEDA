85dccbacaf5d91510fd38b744acf4502f340a8d2
Yuki Furuya <yuki.furuya.4m@stu.hosei.ac.jp>
2017-11-07 23:15:17 +0900

    validated yosys for Centos7

--- yosys/recipes/default.rb
+++ yosys/recipes/default.rb
@@ -16,7 +16,11 @@ case node[:platform]
 when 'ubuntu'
   packages = ['clang', 'python3', 'tcl-dev', 'libreadline-dev', 'libffi-dev', 'mercurial-git', 'gawk']
 when 'centos'
-  packages = ['wget', 'tcl-devel', 'readline-devel', 'bison', 'libffi-devel', 'gcc', 'zip']
+  if node['platform_version'].to_i == 6 then
+    packages = ['wget','tcl-devel', 'readline-devel','bison','libffi-devel','gcc','zip']
+  elsif node['platform_version'].to_i == 7 then
+    packages = ['tcl-devel', 'readline-devel','bison','libffi-devel','gcc','zip','git-hg','make']
+  end
 end
 
 packages.each{|p|
@@ -24,52 +28,65 @@ packages.each{|p|
     action :install
   end
 }
+
 case node[:platform]
 when 'centos'
-  remote_file '/usr/local/src/gcc-4.8.5.tar.gz' do
-    source 'http://www.netgull.com/gcc/releases/gcc-4.8.5/gcc-4.8.5.tar.gz'
-    mode '0755'
-  end
-
-  bash 'extract gcc' do
-    cwd '/usr/local/src'
-    code <<-EOH
-      tar xzf /usr/local/src/gcc-4.8.5.tar.gz
-    EOH
-    not_if { ::File.exist? '/usr/local/src/gcc-4.8.5' }
-  end
-
-  bash 'update gcc' do
-    cwd '/usr/local/src/gcc-4.8.5'
-    code <<-EOF
-      ./contrib/download_prerequisites
-      ./configure --prefix=/usr --disable-bootstrap --disable-multilib
-      make && make install
-    EOF
-  end
-
   bash 'install ius' do
     code <<-EOF
       curl -s https://setup.ius.io/|sh
     EOF
-    not_if 'rpm -qa |grep ius-release*' 
+    not_if 'rpm -qa |grep ius-release*'
   end
-  packages = ['hg-git', 'python35u']
+  
+  packages = ['python35u']
     packages.each{|p|
       package p do
         action :install
       end
-    }  
+  }
+  
   link '/usr/bin/python3' do
     to '/usr/bin/python3.5'
   end
 
-  bash 'change verilog_parser.y' do
-    cwd '/usr/local/src/yosys/frontends/verilog'
-    code <<-EOF
-      sed -i -e "142,143s:^:/*:" /usr/local/src/yosys/frontends/verilog/verilog_parser.y
-      sed -i -e "142,143s:$:*/:" /usr/local/src/yosys/frontends/verilog/verilog_parser.y
-    EOF
+  if node['platform_version'].to_i == 6 then
+    packages = ['hg-git']
+    packages.each{|p|
+      package p do
+        action :install
+      end
+  }
+  
+    remote_file '/usr/local/src/gcc-4.8.5.tar.gz' do
+      source 'http://www.netgull.com/gcc/releases/gcc-4.8.5/gcc-4.8.5.tar.gz'
+      mode '0755'
+    end
+
+    bash 'extract gcc' do
+      cwd '/usr/local/src'
+      code <<-EOH
+        tar xzf /usr/local/src/gcc-4.8.5.tar.gz
+      EOH
+      not_if { ::File.exist? '/usr/local/src/gcc-4.8.5' }
+    end
+
+    bash 'update gcc-4.8.5' do
+      cwd '/usr/local/src/gcc-4.8.5'
+      code <<-EOF
+        ./contrib/download_prerequisites
+        ./configure --prefix=/usr --disable-bootstrap --disable-multilib
+        make && make install
+      EOF
+      not_if 'rpm -qa |grep gcc-4.8.5*'
+    end
+   
+    bash 'change verilog_parser.y' do
+     cwd '/usr/local/src/yosys/frontends/verilog'
+     code <<-EOF
+       sed -i -e "142,143s:^:/*:" /usr/local/src/yosys/frontends/verilog/verilog_parser.y
+       sed -i -e "142,143s:$:*/:" /usr/local/src/yosys/frontends/verilog/verilog_parser.y
+     EOF
+    end
   end
 end
 
