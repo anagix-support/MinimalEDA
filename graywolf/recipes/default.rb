cd4dde70571d73206d5c614645ed4676dfb81a9a
root <root@ip-172-31-0-134.ap-northeast-1.compute.internal>
2018-02-19 14:36:44 +0000

    Unbuntu 14.04 support in graywolf recipe 0.1.1

--- graywolf/recipes/default.rb
+++ graywolf/recipes/default.rb
@@ -16,7 +16,11 @@ end
 
 case node[:platform]
 when 'ubuntu', 'debian'
-  packages = ['cmake', 'libgsl-dev', 'libx11-dev', 'pkg-config']
+  if node[:platform_version] == '14.04'
+    packages = ['cmake', 'libgsl0-dev', 'libx11-dev', 'pkg-config']
+  else
+    packages = ['cmake', 'libgsl-dev', 'libx11-dev', 'pkg-config']
+  end
 when 'centos'
   packages = ['gsl-devel', 'libX11-devel', 'cmake', 'gcc', 'gcc-c++', 'make']
 end
